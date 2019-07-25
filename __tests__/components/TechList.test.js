845dd064d4377d80e63bca8372d5c1d9d69c0400
Diogo <drd.rclrd@gmail.com>
2019-07-25 19:59:20 -0300

    test for get text in button

---
+++ __tests__/components/TechList.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import TechList from '~/components/TechList';
+
+describe('TechList component', () => {
+  it('should be able to add new tech', () => {
+    const { getByText, getByTestId, getByLabelText } = render(<TechList />);
+
+    // fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    // fireEvent.submit(getByTestId('tech-form')); 
+
+    fireEvent.click(getByText('Adicionar'));
+
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+  });
+});
