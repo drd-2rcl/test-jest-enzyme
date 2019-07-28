a647ae4cc3cef9d4a2878d5b4281d9aa1b9b3c8c
Diogo <drd.rclrd@gmail.com>
2019-07-27 22:29:53 -0300

    :recycle: refactoring

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 export default function TechList() {
-  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const dispatch = useDispatch();
+  const techs = useSelector(state = state.techs);
 
   function handleAddTech() {
-    setTechs([ ...techs, 'Node.js'])
+    dispatch({ type: 'ADD_TECH', payload: { tech: newTech } })
+    setNewTech('');
   }
   
   return (
-    <div>
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
       <ul data-testid="tech-list">
         {techs.map(tech => <li key="tech">{tech}</li>)}
       </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+      
       <button onClick={handleAddTech}>Adicionar</button>
-    </div>
+    </form>
   )
 }
