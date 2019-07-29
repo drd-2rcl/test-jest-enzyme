6ec241eb18ec08e21e50557b4558ccca3414c11c
Diogo <drd.rclrd@gmail.com>
2019-07-29 09:27:08 -0300

    change data for isolate file

--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { addTech } from '../../store/modules/techs/actions'
 
 export default function TechList() {
   const [newTech, setNewTech] = useState('');
@@ -8,8 +9,8 @@ export default function TechList() {
   const techs = useSelector(state => state.techs);
 
   function handleAddTech() {
-    dispatch({ type: 'ADD_TECH', payload: { tech: newTech } });
-    
+    dispatch(addTech(newTech));
+
     setNewTech('');
   }
   
